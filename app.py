0fa42d43c8488872b7a90ed93a03c6e52bd8734a
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2023-09-04 17:38:50 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,6 @@
 from flask import *
 from main import calculation_with_metadata
 import requests
-from pytube import YouTube, streams
-import iGetMusic as iGet
-from mutagen.easyid3 import EasyID3
-from mutagen.id3 import ID3, APIC, TIT2
-import eyed3
-from eyed3.id3.frames import ImageFrame
-from mutagen.mp3 import MP3
-from moviepy.editor import VideoFileClip
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-import os
-from pathlib import Path
 
 app = Flask(__name__, template_folder="templates")
 app.config["DEBUG"] = True
